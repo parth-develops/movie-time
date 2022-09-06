c7a8612a7416e5f21e4ce8b915dc1b8c8074931e
Parth Umrania <pumrania9@gmail.com>
2022-09-06 19:13:16 +0530

    Developed MovieDetails page with dummy content

--- src/components/MovieInfo.tsx
+++ src/components/MovieInfo.tsx
@@ -9,6 +9,28 @@ const MovieInfo = () => {
         </div>
         <div className={classes['movie-details']}>
           <h2 className={classes['movie-title']}>DC League of Super-Pets(Released)</h2>
+          <div className='plot'>
+            <h4>PLOT</h4>
+            <p className={classes['movie-plot']}>When Superman and the rest of the Justice League are kidnapped, Krypto the Super-Dog must convince a rag-tag shelter pack - Ace the hound, PB the potbellied pig, Merton the turtle and Chip the squirrel - to master their own newfound powers and help him rescue the superheroes.</p>
+          </div>
+          <div className='genre'>
+            <h4>GENRES</h4>
+            <ul className={classes.genres}>
+              <li>Animation</li>
+              <li>Animation</li>
+              <li>Animation</li>
+              <li>Animation</li>
+              <li>Animation</li>
+            </ul>
+          </div>
+          <div className="rating">
+            <h4>IMBD RATING</h4>
+            <p>5.267</p>
+          </div>
+          <div className='extra-info'>
+            <h4>DIRECTOR</h4>
+            <p>Jared Stern</p>
+          </div>
         </div>
       </div>
     </section>
--- src/pages/MovieDetails.tsx
+++ src/pages/MovieDetails.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import MovieBanner from "../components/MovieBanner";
+import MovieInfo from "../components/MovieInfo";
 import classes from './MovieDetails.module.css';
 
 const MovieDetails = () => {
   return (
     <div className={classes.MovieDetails}>
       <MovieBanner />
+      <MovieInfo/>
     </div>
   );
 };
